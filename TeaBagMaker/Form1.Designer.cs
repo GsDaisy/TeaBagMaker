443ce7317747c31ee3cca6a462328c73aa442f63
GsDaisy <42744032+GsDaisy@users.noreply.github.com>
2019-05-27 09:59:33 +0900

    #2 ComboBox 선택에 따른 Timer 기능, TextBox 남은 시간 분, 초 단위로 나타내기 구현 완료

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtCountDown = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -43,6 +43,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(245, 20);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
@@ -52,17 +53,19 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "담그기!";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // textBox1
+            // txtCountDown
             // 
-            this.textBox1.Location = new System.Drawing.Point(16, 171);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(241, 21);
-            this.textBox1.TabIndex = 2;
+            this.txtCountDown.Location = new System.Drawing.Point(16, 171);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.Size = new System.Drawing.Size(241, 21);
+            this.txtCountDown.TabIndex = 2;
             // 
             // timer1
             // 
             this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // label1
             // 
@@ -79,11 +82,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(339, 307);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtCountDown);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,7 +97,7 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCountDown;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
     }
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -11,9 +11,85 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        private int CountOrgNum;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("홍차");
+            comboBox1.Items.Add("녹차");
+            comboBox1.Items.Add("루이보스차");
+            comboBox1.Items.Add("국화차");
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            String sel = comboBox1.SelectedItem.ToString();
+
+            if (sel.Equals("홍차"))
+            {
+
+                this.CountOrgNum = 120;
+                this.timer1.Enabled = true;
+            }
+            else if (sel.Equals("녹차"))
+            {
+
+                this.CountOrgNum = 180;
+                this.timer1.Enabled = true;
+            }
+            else if (sel.Equals("루이보스차"))
+            {
+
+                this.CountOrgNum = 300;
+                this.timer1.Enabled = true;
+            }
+            else 
+            {
+
+                this.CountOrgNum = 120;
+                this.timer1.Enabled = true;
+            }
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.CountOrgNum < 1)
+            {
+                this.timer1.Enabled = false;
+                MessageBox.Show("티백을 건지세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtCountDown.Text = "";
+
+                this.comboBox1.Focus();
+            }
+            else
+            {
+                this.CountOrgNum = this.CountOrgNum - 1;
+                int mi = CountOrgNum / 60;
+                int se = CountOrgNum - (mi*60);
+
+                if (mi == 0)
+                {
+                    this.txtCountDown.Text = se + "초 남았습니다.";
+                }
+                else
+                {
+                    //this.txtCountDown.Text = Convert.ToString(this.CountOrgNum);
+                    this.txtCountDown.Text = mi + "분 " + se + "초 남았습니다.";
+                }
+            }
         }
     }
 }
